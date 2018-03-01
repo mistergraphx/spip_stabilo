03aedc2e18a847d893269a7c188a07627976f1b0
Mist. GraphX <arnaud.berard@mister-graphx.com>
2018-03-01 11:56:05 +0100

    typo dans l'install de base --shame--

--- stabilo_administrations.php
+++ stabilo_administrations.php
@@ -10,17 +10,17 @@ function stabilo_upgrade($nom_meta_base_version, $version_cible) {
 
 	$stablilo_colors = array(
 		'stabilo1'=> array(
-				'label'=> 'Description de la couleure de surlignage 1',
+				'label'=> 'Description de la couleur de surlignage 1',
 				'background'=> '#9e9e9e',
 				'foreground'=> '#000000',
 		),
 		'stabilo2'=> array(
-				'label'=> 'Description de la couleure de surlignage 1',
+				'label'=> 'Description de la couleur de surlignage 1',
 				'background'=> '#ffeb3b',
 				'foreground'=> '#000000',
 		),
 		'stabilo3'=> array(
-				'label'=> 'Description de la couleure de surlignage 1',
+				'label'=> 'Description de la couleur de surlignage 1',
 				'background'=> '#e44b7f',
 				'foreground'=> '#000000',
 		),
